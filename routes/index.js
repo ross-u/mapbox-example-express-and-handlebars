7980490dbef1a95f78510dd7efbdf87dac079e42
ross-u <cirkovic.uros.srb@gmail.com>
2020-05-01 09:57:25 +0200

    Update comments

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Restaurants = require('../models/restaurant');
+const Restaurants = require("../models/restaurant");
 
-// GET      /   Render home page with the restaurants map */
-router.get('/', (req, res, next) => {
+// GET      /     - Renders the home page with the restaurants map
+router.get("/", (req, res, next) => {
   Restaurants.find()
     .then((restaurants) => {
       console.log(restaurants[0].location.coordinates[0]);
-      res.render('index', { restaurants });
+      res.render("index", { restaurants });
     })
     .catch(next);
 });
